9146df3eb3ee01e2c7c9068f0f686721268344bb
Huy <phanngochuy.92.2017.2018@gmail.com>
2024-11-17 21:47:11 +0700

    Fetch data with firebase

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,46 +1,24 @@
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// import * as firebase from "firebase";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
 
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyC7MRMpeSTeeUUS8l7qZEiBTqKeRl7XZXk",
-//   authDomain: "fir-reactnative-55e4a.firebaseapp.com",
-//   projectId: "fir-reactnative-55e4a",
-//   storageBucket: "fir-reactnative-55e4a.firebasestorage.app",
-//   messagingSenderId: "148871610552",
-//   appId: "1:148871610552:web:edf94be0a7dfedfe98f44d",
-//   measurementId: "G-Z0PKF8R6D6"
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
-
-// export const firebaseApp = firebase.initializeApp(firebaseConfig);
-// Import the required functions from Firebase SDK v9+
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth"; // For Authentication
 import { getAnalytics } from "firebase/analytics"; // Optional if you use Analytics
+import { getFirestore } from "firebase/firestore";
 
 // Your Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyC7MRMpeSTeeUUS8l7qZEiBTqKeRl7XZXk",
   authDomain: "fir-reactnative-55e4a.firebaseapp.com",
   projectId: "fir-reactnative-55e4a",
-  storageBucket: "fir-reactnative-55e4a.appspot.com",
+  storageBucket: "fir-reactnative-55e4a.firebasestorage.app",
   messagingSenderId: "148871610552",
   appId: "1:148871610552:web:edf94be0a7dfedfe98f44d",
-  measurementId: "G-Z0PKF8R6D6",
+  measurementId: "G-Z0PKF8R6D6"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app); // Optional
 const auth = getAuth(app); // Firebase Authentication
-
-export { app, auth };
+const db = getFirestore(app);
+console.log("Firestore DB initialized:", db);
+export { app, auth, db };
